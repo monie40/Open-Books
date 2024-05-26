fe5118049fa58dd98d35d0e6557bef6ef0d0e0f7
Monique Brown <moniquecbrown1@gmail.com>
2024-05-26 11:42:39 -0400

    Started pulling in API data

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import BookCard from './features/books/BookCard';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://openlibrary.org/search.json?title=the+lord+the+rings&limit=2')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }, []);
+
+  console.log(data);
+  const books = JSON.stringify(data);
+  //console.log(books);
+  
   return (
     <>
         <Header />
         <BookCard />
+        <h4>{data.numFound}</h4>
+         
     </> 
-       
-     
   );
 }
 
